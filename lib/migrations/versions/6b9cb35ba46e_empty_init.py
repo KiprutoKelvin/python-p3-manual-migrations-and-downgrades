2cfedc171ddc021abd31ec999c07507fe995da92
iamkipruto <kelvin.kipruto@student.moringaschool.com>
2023-06-13 22:54:33 +0300

    Manual migrations

--- lib/migrations/versions/6b9cb35ba46e_empty_init.py
+++ lib/migrations/versions/6b9cb35ba46e_empty_init.py
@@ -1,24 +1,24 @@
-"""Empty Init
-
-Revision ID: 6b9cb35ba46e
-Revises: 
-Create Date: 2022-08-04 13:21:26.936909
-
+"""Renaming students to scholars
+Revision ID: 121debf245fe
+Revises: 791279dd0760
+Create Date: 2023-06-06 01:41:14.959244
 """
 from alembic import op
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6b9cb35ba46e'
-down_revision = None
+revision = '121debf245fe'
+down_revision = '791279dd0760'
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    pass
+    op.rename_table('students', 'scholars')
+    op.alter_column('students', 'name', student_name='student_name')
 
 
 def downgrade() -> None:
-    pass
+    op.rename_table('scholars', 'students')
+    op.alter_column('students', 'student_name', student_name='name')
